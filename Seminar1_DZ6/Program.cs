6b0305f106c2ee4c4e9c96b01a44822e095fa7d9
Евгений Кириллов <KirillovEE@pssochi.ru>
2022-11-11 15:12:56 +0300

    save4_DZ

---
+++ Seminar1_DZ6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Привет, введи число!");
+int A = Convert.ToInt32(Console.ReadLine());
+if(A % 2 == 0) 
+{
+    Console.Write(A + " -> Да");
+}
+else
+{
+    Console.Write(A + " -> Нет");
+}
