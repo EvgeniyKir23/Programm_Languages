1b1f9d9fa8b22133cc8652dbd34773ac76c4da45
Евгений Кириллов <KirillovEE@pssochi.ru>
2022-11-11 14:56:33 +0300

    save_save

--- Seminar1_DZ2/Program.cs
+++ Seminar1_DZ2/Program.cs
@@ -1,13 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Привет, введи 3 числа!");
+﻿
+Console.WriteLine("Привет, введи 2 числа!");
 
 int A = Convert.ToInt32(Console.ReadLine());
 int B = Convert.ToInt32(Console.ReadLine());
-int C = Convert.ToInt32(Console.ReadLine());
 int max = A;
 if(A > max) max = A;
 if(B > max) max = B;
-if(C > max) max = C;
+int min = A;
+if(A < min) min = A;
+if(B < min) min = B;
 Console.Write("max = ");
 Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
