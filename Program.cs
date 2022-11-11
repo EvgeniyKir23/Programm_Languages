6d766b02f3b4e8150cdc6aa713b2b3a639f12d2c
Евгений Кириллов <KirillovEE@pssochi.ru>
2022-11-11 14:32:28 +0300

    Практическое задание 1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Привет, введи 2 числа!");
+
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int max = A;
+if(A > max) max = A;
+if(B > max) max = B;
+Console.Write("max = ");
+Console.WriteLine(max);
